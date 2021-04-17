9cb61e43aaeec948e15ef485f03c9693284a50c4
Stanisław Straburzyński <sstraburzynski@gmail.com>
2021-04-17 21:37:59 +0200

    Introductory EDA for "ECG200"

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from typing import Union
 
 sns.set_style("darkgrid")
 dir_path = "datasets/UCRArchive_2018"
@@ -22,20 +23,45 @@ def get_path(
     return path
 
 
+def x_y_split(
+    df: pd.DataFrame,
+    y_col: str
+):
+    return df.drop([y_col], axis=1), df[y_col]
+
+
 def get_dataset(
     ds: str,
     dir_path: str = dir_path,
     ext: str = "tsv",
-    sep: str = '\t'
+    sep: str = '\t',
+    train_test_split: bool = True,
+    x_y_split: bool = False,
+    y_col: Union[str, int] = None
 ):
-    train_df = pd.read_csv(
+    train = pd.read_csv(
         get_path(ds, dir_path=dir_path, train=True),
         sep=sep,
         header=None
         )
-    test_df = pd.read_csv(
+    test = pd.read_csv(
         get_path(ds, dir_path=dir_path, train=False),
         sep=sep,
         header=None
         )
-    return train_df, test_df
+    if x_y_split:
+        if y_col is None:
+            raise ValueError("\"y_col\" must be defined")
+        if train_test_split:
+            X_train, y_train = x_y_split(train, y_col)
+            X_test, y_test = x_y_split(test, y_col)
+            result = ((X_train, y_train), (X_test, y_test))
+        else:
+            result = pd.concat([train, test])
+            result = x_y_split(result, y_col)
+    else:
+        if train_test_split:
+            result = train, test
+        else:
+            result = pd.concat([train, test])
+    return result
